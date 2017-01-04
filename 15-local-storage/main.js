b0524a5822c9411e6ad290d5583930dcac2193d6
Deyan Yanakiev <kliomba@abv.bg>
2017-01-04 10:28:09 +0200

    Day15 - refactor local storage access

--- 15-local-storage/main.js
+++ 15-local-storage/main.js
@@ -3,13 +3,30 @@
 
   const itemsList = document.querySelector('.plates');
   const addItems = document.querySelector('.add-items');
-  const items = JSON.parse(localStorage.getItem('plates')) || [];
 
-  const addItem = (text) => {
-    const item = { text: text, done: false };
+  const items = {
+    get() {
+      return JSON.parse(localStorage.getItem('plates')) || [];
+    },
+    add(text) {
+      const item = { text: text, done: false };
+      const items = this.get();
 
-    items.push(item);
-    localStorage.setItem('plates', JSON.stringify(items));
+      items.push(item);
+      localStorage.setItem('plates', JSON.stringify(items));
+    },
+    remove(index) {
+      const items = this.get();
+
+      items.splice(index, 1);
+      localStorage.setItem('plates', JSON.stringify(items));
+    },
+    toggleDone(index) {
+      const items = this.get();
+
+      items[index].done = !items[index].done;
+      localStorage.setItem('plates', JSON.stringify(items));
+    }
   };
 
   const removeChildren = (node) => {
@@ -22,14 +39,13 @@
     const fragment = document.createDocumentFragment();
     const element = document.createElement('li');
 
-    removeChildren(list);
     items.forEach((item, i) => {
       const clone = element.cloneNode();
 
       clone.innerHTML = `
         <input type="checkbox" data-index="${i}" id="plate-${i}" ${item.done ? 'checked': ''}/>
         <label for="plate-${i}">${item.text}</label>
-        <span class="delete" data-index="${i}">x</span>
+        <span class="delete" data-index="${i}">&times;</span>
       `;
       fragment.appendChild(clone);
     });
@@ -37,29 +53,37 @@
     list.appendChild(fragment);
   };
 
-  renderList(itemsList, items);
+  const updateList = (list, items) => {
+    removeChildren(list);
+    renderList(list, items);
+  };
+
 
   itemsList.addEventListener('change', (e) => {
     const index = e.target.dataset.index;
 
-    items[index].done = e.target.checked;
-    localStorage.setItem('plates', JSON.stringify(items));
+    items.toggleDone(index);
   });
 
   itemsList.addEventListener('click', (e) => {
+    if (!e.target.matches('.delete')) {
+      return;
+    }
+
     const index = e.target.dataset.index;
 
-    items.splice(index, 1);
-    renderList(itemsList, items);
-    localStorage.setItem('plates', JSON.stringify(items));
+    items.remove(index);
+    updateList(itemsList, items.get());
   });
 
   addItems.addEventListener('submit', (e) => {
     const text = e.target.querySelector('input[type="text"]').value;
 
-    addItem(text);
-    renderList(itemsList, items);
+    items.add(text);
+    updateList(itemsList, items.get());
     e.preventDefault();
     e.target.reset();
   });
+
+  document.addEventListener('DOMContentLoaded', () => updateList(itemsList, items.get()));
 }(document));
