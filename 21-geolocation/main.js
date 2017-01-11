b5eb0bb19ee33af7279c4456d2e0f9b263d94d68
Deyan Yanakiev <kliomba@abv.bg>
2017-01-11 11:25:02 +0200

    Day21 - geolocation and google maps

---
+++ 21-geolocation/main.js
@@ -0,0 +1,50 @@
+(function (navigator, window, document) {
+  'use strict';
+
+  const api = 'https://maps.googleapis.com/maps/api/js';
+  const key = 'AIzaSyCD32FcA23Lw1ulQJGA5ROxyP2xcYKQZuQ';
+  let coords = { lat: 0, lng: 0};
+
+  window.createMap = () => {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: coords,
+      styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {featureType: 'administrative.locality', elementType: 'labels.text.fill', stylers: [{color: '#d59563'}]},
+        {featureType: 'poi', elementType: 'labels.text.fill', stylers: [{color: '#d59563'}]},
+        {featureType: 'poi.park', elementType: 'geometry', stylers: [{color: '#263c3f'}]},
+        {featureType: 'poi.park', elementType: 'labels.text.fill', stylers: [{color: '#6b9a76'}]},
+        {featureType: 'road', elementType: 'geometry', stylers: [{color: '#38414e'}]},
+        {featureType: 'road', elementType: 'geometry.stroke', stylers: [{color: '#212a37'}]},
+        {featureType: 'road', elementType: 'labels.text.fill', stylers: [{color: '#9ca5b3'}]},
+        {featureType: 'road.highway', elementType: 'geometry', stylers: [{color: '#746855'}]},
+        {featureType: 'road.highway', elementType: 'geometry.stroke', stylers: [{color: '#1f2835'}]},
+        {featureType: 'road.highway', elementType: 'labels.text.fill', stylers: [{color: '#f3d19c'}]},
+        {featureType: 'transit', elementType: 'geometry', stylers: [{color: '#2f3948'}]},
+        {featureType: 'transit.station', elementType: 'labels.text.fill', stylers: [{color: '#d59563'}]},
+        {featureType: 'water', elementType: 'geometry', stylers: [{color: '#17263c'}]},
+        {featureType: 'water', elementType: 'labels.text.fill', stylers: [{color: '#515c6d'}]},
+        {featureType: 'water', elementType: 'labels.text.stroke', stylers: [{color: '#17263c'}]}
+      ]
+    });
+
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: map
+    });
+  };
+
+  navigator.geolocation.getCurrentPosition(e => {
+    const script = document.createElement('script');
+
+    coords.lat = e.coords.latitude;
+    coords.lng = e.coords.longitude;
+    script.src = `${api}?key=${key}&callback=createMap`;
+
+    document.body.appendChild(script);
+  });
+
+}(navigator, window, document));
